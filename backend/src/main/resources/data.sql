6554005d3c0d9920862ff3f0b7f394364cb5007e
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-29 15:03:27 -0300

    Offer entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQHoHWyiBD9XhNxmJxgnVssf0g3eqttUvsmNOljN5_QyhgDuh5KQtdqKTJ0a7WkRtJMoE&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcbeHBaSUWLcO5i0fW3aRVkTq6CTZFrIsjyL1cOVreua2VOTkFpuiRHDixQfkGugOuXjw&usqp=CAU');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
