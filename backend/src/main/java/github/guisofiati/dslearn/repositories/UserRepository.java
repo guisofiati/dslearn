f0bd4c12a6a22acf138a923fcd82eaba3a28c70e
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-10-05 14:22:19 -0300

    Exceptions, validation and security

--- backend/src/main/java/github/guisofiati/dslearn/repositories/UserRepository.java
+++ backend/src/main/java/github/guisofiati/dslearn/repositories/UserRepository.java
@@ -7,4 +7,6 @@ import github.guisofiati.dslearn.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByEmail(String email);
 }
---
+++ backend/src/main/java/github/guisofiati/dslearn/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package github.guisofiati.dslearn.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
