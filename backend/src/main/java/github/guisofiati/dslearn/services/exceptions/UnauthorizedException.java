0e2b63f4f748d3ea0c7e500549885cd1c08ca22d
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-10-05 16:30:51 -0300

    Custom service level authorization

---
+++ backend/src/main/java/github/guisofiati/dslearn/services/exceptions/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package github.guisofiati.dslearn.services.exceptions;
+
+public class UnauthorizedException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public UnauthorizedException(String msg) {
+		super(msg);
+	}
+}
