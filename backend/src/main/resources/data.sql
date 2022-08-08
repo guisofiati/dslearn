e80ae976825d0840f772a94458311750968e8ed5
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-08-08 14:34:28 -0300

    Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,3 +21,7 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://bit.ly/3p3evVD', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas duvidas', 2, 'https://bit.ly/3p3evVD', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://bit.ly/3p3evVD', 0, 1);
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_Id, prerequisite_Id) VALUES ('Capitulo 1: Introdução', 'Capitulo inicial para apresentação do curso', 1, 'https://bit.ly/3p3evVD', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_Id, prerequisite_Id) VALUES ('Capitulo 2: Tags', 'Aprendendo sobre as tags HTML', 2, 'https://bit.ly/3p3evVD', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_Id, prerequisite_Id) VALUES ('Capitulo 3: Atributos', 'Vamos aprender sobre os atributos que cada tag possue', 3, 'https://bit.ly/3p3evVD', 1, 2);
