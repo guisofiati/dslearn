4276b19b0ff48de2fbb880f8aaac6b7ff4633a95
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-08-08 10:55:12 -0300

    Offer and many to one association

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://bit.ly/3p3evVD', 'https://bit.ly/3BQD6om');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-05-05T10:00:00Z', TIMESTAMP WITH TIME ZONE '2023-05-05T10:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-12-05T10:00:00Z', TIMESTAMP WITH TIME ZONE '2023-12-05T10:00:00Z', 1);
