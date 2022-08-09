d3057584964b65fb2066a95e924cded4b687c6e0
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-08-09 09:47:41 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, resource_Id, prerequisite_Id) VALUES ('Capitulo 1: Introdução', 'Capitulo inicial para apresentação do curso', 1, 'https://bit.ly/3p3evVD', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_Id, prerequisite_Id) VALUES ('Capitulo 2: Tags', 'Aprendendo sobre as tags HTML', 2, 'https://bit.ly/3p3evVD', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_Id, prerequisite_Id) VALUES ('Capitulo 3: Atributos', 'Vamos aprender sobre os atributos que cada tag possue', 3, 'https://bit.ly/3p3evVD', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2023-05-05T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2023-05-05T13:00:00Z', null, true, false);
